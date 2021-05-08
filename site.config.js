02b416d667e04c2feaa93187a33fdcee65f8fded
co <51730273+c0ralinee@users.noreply.github.com>
2021-05-08 13:36:57 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -8,7 +8,7 @@ module.exports = {
 
   // basic site info (required)
   name: "Co's Box",
-  domain: 'https://www.notion.so/Co-s-Blog-a70321a892c24d9ca05f0c51b80b9934',
+  domain: 'https://www.notion.so/Coraline-s-Blog-048b22c92ec248609e05784149906ca1',
   author: 'Coraline',
 
   // open graph metadata (optional)
